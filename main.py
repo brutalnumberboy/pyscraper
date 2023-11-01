3e7bb9d5435ebb126fdf8b2df2812db23a727a18
brutalnumberboy <scandalzx@protonmail.com>
2023-11-01 18:53:48 +0100

    added func which scrapes links, reformatted the parser and added subparsers

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 
 
-def copy_content(file):
-    with open(file, 'r') as fp:
+def copy_content(args):
+    with open(args, 'r') as fp:
         soup = BeautifulSoup(fp, 'html.parser')
     with open('result.html', 'a') as fp:
         fp.write(soup.prettify())
 
 
+def find_links(args):
+    with open(args, 'r') as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    with open('result.txt', 'a') as fp:
+        for link in soup.find_all('a', href=True):
+            fp.write(link['href'] + '\n')
+
+
+parser = ArgumentParser()
+subparser = parser.add_subparsers(required=True)
+
+parser_html = subparser.add_parser('html', help="Writes the html document into result.html")
+parser_html.add_argument('-f', type=str, dest="filename")
+parser_html.set_defaults(func=copy_content)
+
+parser_links = subparser.add_parser('links', help="Extracts links from the documents and writes them into result.txt")
+parser_links.add_argument('-l', type=str, dest="filename")
+parser_links.set_defaults(func=find_links)
+
+
+
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("--html", dest="filename",
-                        help='Download an html file of the website', type=str)
     args = parser.parse_args()
-    copy_content(args.filename)
+    args.func(args.filename)
