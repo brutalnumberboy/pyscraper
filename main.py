d00aa348d7ff0323a5bcc7554d6ed46ab8686af3
brutalnumberboy <scandalzx@protonmail.com>
2023-11-01 14:41:11 +0100

    added ability to pass command-line arguments

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from bs4 import BeautifulSoup
+from argparse import ArgumentParser
 
 
 def copy_content(file):
     with open(file, 'r') as fp:
         soup = BeautifulSoup(fp, 'html.parser')
-    with open('result.html', 'a') as file:
-        file.write(soup.prettify())
+    with open('result.html', 'a') as fp:
+        fp.write(soup.prettify())
 
+
+def search_links(file):
+    with open(file, 'r') as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    with open('result.html', 'a') as fp:
+        for link in soup.find_all('a'):
+            fp.write(link.getText('href') + '\n')
 
 
 if __name__ == '__main__':
-    print("Welcome to pyscraper, please enter the filename: ")
-    copy_content(input())
+    parser = ArgumentParser()
+    parser.add_argument("--html", dest="filename",
+                        help='Download an html file of the website', type=str)
+    args = parser.parse_args()
+    copy_content(args.filename)
