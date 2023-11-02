cb7720f255933313df34ecfd7828760770bd46b9
brutalnumberboy <scandalzx@protonmail.com>
2023-11-02 15:49:58 +0100

    now can look for images and parse url

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
+import httplib2
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 
+http = httplib2.Http()
+status, response = http.request('https://news.ycombinator.com/') # take input url????
 
-def copy_content(args):
-    with open(args, 'r') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+
+def copy_content():
+    soup = BeautifulSoup(response, 'html.parser')
     with open('result.html', 'a') as fp:
         fp.write(soup.prettify())
 
 
-def find_links(args):
-    with open(args, 'r') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+def find_links():
+    soup = BeautifulSoup(response, 'html.parser')
     with open('result.txt', 'a') as fp:
         for link in soup.find_all('a', href=True):
             fp.write(link['href'] + '\n')
 
+def find_img():
+    soup = BeautifulSoup(response, 'html.parser')
+    with open('images.txt', 'a') as fp:
+        for img in soup.find_all('img'):
+            fp.write(img['src'] + '\n')
 
 parser = ArgumentParser()
 subparser = parser.add_subparsers(required=True)
 
 parser_html = subparser.add_parser('html', help="Writes the html document into result.html")
-parser_html.add_argument('-f', type=str, dest="filename")
+# parser_html.add_argument('-f', type=str, dest="filename")
 parser_html.set_defaults(func=copy_content)
 
 parser_links = subparser.add_parser('links', help="Extracts links from the documents and writes them into result.txt")
-parser_links.add_argument('-l', type=str, dest="filename")
+# parser_links.add_argument('-l', type=str, dest="filename")
 parser_links.set_defaults(func=find_links)
+
+parser_links = subparser.add_parser('images', help="Extracts links from the documents and writes them into result.txt")
+# parser_links.add_argument('-i', type=str, dest="filename")
+parser_links.set_defaults(func=find_img)
 
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    args.func(args.filename)
+    args.func()
+    # idk how but it works
