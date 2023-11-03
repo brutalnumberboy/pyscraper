98dda3692b8d34d08562db6f285a7b8cd0b42791
brutalnumberboy <scandalzx@protonmail.com>
2023-11-03 16:51:43 +0100

    user can now specify the url

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 
 http = httplib2.Http()
-status, response = http.request('https://news.ycombinator.com/') # take input url????
-soup = BeautifulSoup(response, 'html.parser')
 
-
-def copy_content():
+def copy_content(uri):
+    status, response = http.request(uri, "GET") 
+    soup = BeautifulSoup(response, 'html.parser')
     with open('result.html', 'a') as fp:
         fp.write(soup.prettify())
 
 
-def find_links():
+def find_links(uri):
+    status, response = http.request(uri, "GET") 
+    soup = BeautifulSoup(response, 'html.parser')
     with open('result.txt', 'a') as fp:
         for link in soup.find_all('a', href=True):
             fp.write(link['href'] + '\n')
 
-def find_img():
+def find_img(uri):
+    status, response = http.request(uri, "GET") 
+    soup = BeautifulSoup(response, 'html.parser')
     with open('images.txt', 'a') as fp:
         for img in soup.find_all('img'):
             fp.write(img['src'] + '\n')
@@ -26,20 +29,19 @@ parser = ArgumentParser()
 subparser = parser.add_subparsers(required=True)
 
 parser_html = subparser.add_parser('html', help="Writes the html document into result.html")
-# parser_html.add_argument('-f', type=str, dest="filename")
+parser_html.add_argument('-l', type=str, dest="url", help="Specify the link")
 parser_html.set_defaults(func=copy_content)
 
 parser_links = subparser.add_parser('links', help="Extracts links from the documents and writes them into result.txt")
-# parser_links.add_argument('-l', type=str, dest="filename")
+parser_links.add_argument('-l', type=str, dest="url", help="Specify the link")
 parser_links.set_defaults(func=find_links)
 
 parser_links = subparser.add_parser('images', help="Extracts links from the documents and writes them into result.txt")
-# parser_links.add_argument('-i', type=str, dest="filename")
+parser_links.add_argument('-l', type=str, dest="url", help="Specify the link")
 parser_links.set_defaults(func=find_img)
 
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    args.func()
-    # idk how but it works
+    args.func(args.url)
